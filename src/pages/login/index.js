e33b0f09c0825d80024b1b75757e532c1bd4b2ad
Luis Henrique <luishenrique.mf01@gmail.com>
2020-08-10 21:18:59 -0300

    Configured isLoading

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { isEmail } from 'validator';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
 import * as actions from '../../store/modules/auth/actions';
 
+import Loading from '../../components/Loading';
+
 export default function Login(props) {
   const dispatch = useDispatch();
 
   const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const isLoading = useSelector((state) => state.auth.isLoading);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -25,7 +29,7 @@ export default function Login(props) {
       toast.error('E-mail inválido');
     }
 
-    if (password.length < 3 || password.length > 50) {
+    if (password.length < 6 || password.length > 50) {
       formErrors = true;
       toast.error('Senha inválida');
     }
@@ -37,7 +41,8 @@ export default function Login(props) {
 
   return (
     <Container>
-      <h1>Login {email}</h1>
+      <Loading isLoading={isLoading} />
+      <h1>Login</h1>
 
       <Form onSubmit={handleSubmit}>
         <input
